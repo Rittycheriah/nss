261e0d5e856c5ed93298b9cf76058d2371b16ccc
Steve Brownlee <chortlehoort@gmail.com>
2015-03-28 19:24:19 -0500

    Comments

--- week9/jasmine/spec/TransmogrifierSpec.js
+++ week9/jasmine/spec/TransmogrifierSpec.js
@@ -8,6 +8,7 @@ describe("Transmogrifier", function() {
   it("should create an array", function () {
 
     var newArray = transmogrifier.createArray("a","b","c","d");
+
     expect(newArray).toEqual(jasmine.arrayContaining(["a","b"]));
 
   });
@@ -15,6 +16,7 @@ describe("Transmogrifier", function() {
   it("should convert an array to a string", function () {
 
     var newString = transmogrifier.createStringFromArray(["a","b"], "/");
+
     expect(newString).toEqual("a/b");
 
   });
@@ -22,10 +24,11 @@ describe("Transmogrifier", function() {
   it("should create a reverse sorted array", function () {
 
     var newArray = transmogrifier.reverseSort(["a","g","o","c","s","l"]);
+
     expect(newArray[0]).toEqual("s");
     expect(newArray[1]).toEqual("o");
     expect(newArray[2]).toEqual("l");
-
+    // etc....
   });
 
 
